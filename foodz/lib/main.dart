66fbee4e8577afb1cb1362025fd0668433b44a80
fatmata597 <55181515+fatmata597@users.noreply.github.com>
2021-02-16 00:42:20 +0200

    data table

--- foodz/lib/main.dart
+++ foodz/lib/main.dart
@@ -19,3 +19,82 @@ class App extends StatelessWidget {
     );
   }
 }
+
+
+            DataTable(
+              columns: <DataColumn>[
+                DataColumn(
+                  label: Text('Items'),
+                ), //Datacolumn
+                DataColumn(
+                  label: Text('Price'),
+                ), //Datacolumn
+              ], //Datacolumns
+              rows: <DataRow>[
+                DataRow(cells: <DataCell>[
+                  DataCell(
+                    Text('Rice'),
+                  ),
+                  DataCell(
+                    Text(
+                      '3000rwf',
+                      // style: TextStyle(
+                      //   color: Colors.white,
+                      // ),
+                    ),
+                  ),
+                ]), //Datarow
+                DataRow(cells: <DataCell>[
+                  DataCell(
+                    Text('Rice'),
+                  ),
+                  DataCell(
+                    Text(
+                      '3000rwf',
+                      // style: TextStyle(
+                      //   color: Colors.white,
+                      // ),
+                    ),
+                  ),
+                ]), //Datarow
+                DataRow(cells: <DataCell>[
+                  DataCell(
+                    Text('Rice'),
+                  ),
+                  DataCell(
+                    Text(
+                      '30GB',
+                      // style: TextStyle(
+                      //   color: Colors.white,
+                      // ),
+                    ),
+                  ),
+                ]), //Datarow
+                DataRow(cells: <DataCell>[
+                  DataCell(
+                    Text('Rice'),
+                  ),
+                  DataCell(
+                    Text(
+                      '30GB',
+                      // style: TextStyle(
+                      //   color: Colors.white,
+                      // ),
+                    ),
+                  ),
+                ]), //Datarow
+                DataRow(cells: <DataCell>[
+                  DataCell(
+                    Text('Rice'),
+                  ),
+                  DataCell(
+                    Text(
+                      '3000rwf',
+                      // style: TextStyle(
+                      //   color: Colors.white,
+                      // ),
+                    ),
+                  ),
+                ]), //Datarow
+              ], //DataRow
+            ), //Datatables
